4faa5ac07ffbf8fc68b814664ca79c0e3ba78e8c
dsmountreax <dsmountreax@gmail.com>
2021-10-25 15:08:41 -0500

    Before New branch

--- src/test/java/serenityswag/inventory/Lab_37_WhenViewingHighlightedProducts.java
+++ src/test/java/serenityswag/inventory/Lab_37_WhenViewingHighlightedProducts.java
@@ -4,6 +4,7 @@ package serenityswag.inventory;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
@@ -14,14 +15,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 //LAB_19
 @RunWith(SerenityRunner.class)
-public class WhenViewingHighlightedProducts {
+public class Lab_37_WhenViewingHighlightedProducts {
 
     @Managed(driver="chrome") // para correr la prueba con chrome
     WebDriver driver;
 
     @Steps
     LoginActions login;
-    ProductListPageObject productList;
+
+    @Steps
+    ViewProductDetailsActions viewProductDetails;
+
+    ProductList productList;
     ProductDetailsPage productDetails;
 
     @Test
@@ -35,13 +40,31 @@ public class WhenViewingHighlightedProducts {
                 .contains("Sauce Labs Backpack");
     }
 
+    // Agregado  inicio Lab_37
+    @Test
+    public void highLightedProductsShouldDisplayTheCorrespondingImages()
+    {
+        login.as(User.STANDARD_USER);
+        List<String> productOnDisplay=productList.titles();
+
+        SoftAssertions softly= new SoftAssertions();
+
+        productOnDisplay.forEach(
+                productName-> softly.assertThat(productList.imageTextForProduct(productName)).isEqualTo(productName)
+        );
+        softly.assertAll();
+    }
+    // fin agregado Lab_37
+
     //Lab_020
     @Test
     public void shouldDisplayCorrectProductDetailsPage()
     {
         login.as(User.STANDARD_USER);
         String firstItemName=productList.titles().get(0);
-        productList.openProductDetailFor(firstItemName);
+
+        //productList.openProductDetailFor(firstItemName);
+        viewProductDetails.forProductWithName(firstItemName);
         assertThat(productDetails.productName()).isEqualTo(firstItemName);
         productDetails.productImageWithAltValueOf(firstItemName).shouldBeVisible(); // ".inventory_details_container img[alt='Sauce Labs Backpack']"
 
---
+++ src/test/java/serenityswag/inventory/ProductList.java
@@ -0,0 +1,23 @@
+package serenityswag.inventory;
+
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.By;
+
+import java.util.List;
+
+public class ProductList extends PageObject {
+
+    public List<String> titles() {
+        //return findAll(By.className("inventory_item_name")).textContents(); Usual convention
+        return findAll(".inventory_item_name").textContents();
+        // findAll cosa nueva textContents cosa nueva
+    }
+    public static By productDetailsLinkFor(String itemName) {
+        return By.linkText(itemName);
+    }
+
+    public String imageTextForProduct(String productName) {
+        return //find(By.xpath("//div[@class='inventory_item'][contains(.,'"+ productName +"')]//img")).getAttribute("alt");
+                $("//div[@class='inventory_item'][contains(.,'"+ productName +"')]//img").getAttribute("alt");
+    }
+}
